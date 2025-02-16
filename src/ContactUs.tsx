85019d52b2157c69e625bedced8ffaab6f3054a7
Pamode <ebenezerukwe@gmail.com>
2025-02-16 06:01:10 +0100

    Final changes

--- src/ContactUs.tsx
+++ src/ContactUs.tsx
@@ -5,27 +5,27 @@ import contactPic from "../src/assets/contact-img.png"
 
 const ContactUs = () => {
   return (
-    <section id="contact-us" className="md:px-32 sm:px16 px-4 py-16">
+    <section id="contact-us" className="px-32 py-16">
         <h1 className="font-bold text-yellow-400 lg:text-left text-center w-fit">CONTACT US</h1>
         <div className="font-bold text-4xl py-4 col-span-5">Write To Us & Build</div>
-        <div className="grid lg:grid-cols-2 gap-16 py-4">
-            <img src={contactPic} alt="" className="sm:w-full w-3/4 sm:mx-0 mx-auto"/>
-            <div className="grid lg:grid-cols-2 text-center gap-6 sm:w-full w-3/5">
-                <div className="grid border border-transparent xs:border-gray-200 xs:shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
+        <div className="grid lg:grid-cols-2 lg:gap-16 py-4 sm:px-0 px-4">
+            <img src={contactPic} alt="" className="sm:flex hidden"/>
+            <div className="grid lg:grid-cols-2 text-center gap-6">
+                <div className="grid border border-gray-200 shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
                     <BiLocationPlus className="text-3xl mx-auto grid mt-6"/>
                     <h1 className="text-2xl font-semibold">We are here ↓</h1>
                     <h1 className="text-gray-600 text-sm">Nigeria, Africa</h1>
                     <h2 className="text-gray-600 text-sm lg:pb-0 pb-8">BCon, Garden City</h2>
                 </div>
-                <div className="grid border border-transparent xs:border-gray-200 xs:shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
+                <div className="grid border border-gray-200 shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
                     <BsTelephone className="text-3xl mx-auto grid mt-6"/>
                     <h1 className="text-2xl font-semibold">Talk To Us</h1>
                     <h1 className="text-gray-600 text-sm lg:pb-0 pb-8">+234 802 888 4444</h1>
                 </div>
-                <div className="grid border border-transparent xs:border-gray-200 xs:shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
+                <div className="grid border border-gray-200 shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
                     <BiMessage className="text-3xl mx-auto grid mt-6"/>
                     <h1 className="text-2xl font-semibold">Chat With Us</h1>
-                    <div className="text-gray-600 text-sm flex justify-between gap-2 lg:px-18 px-44 lg:pt-0 pt-2  lg:pb-0 pb-8">
+                    <div className="text-gray-600 text-sm flex justify-between lg:px-18 px-44 lg:pt-0 pt-2  lg:pb-0 pb-8">
                         <BsWhatsapp className="hover:text-orange-300 hover:shadow-md"/>
                         <FaFacebookMessenger className="hover:text-orange-300 hover:shadow-md"/>
                         <BsInstagram className="hover:text-orange-300 hover:shadow-md"/>
