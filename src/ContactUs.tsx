9b3e8b41d770c58647bada5f257cd120dd2f6892
Pamode <ebenezerukwe@gmail.com>
2025-02-17 02:55:14 +0100

    Updated contact page

--- src/ContactUs.tsx
+++ src/ContactUs.tsx
@@ -1,34 +1,41 @@
-import { BiLocationPlus, BiMessage } from "react-icons/bi"
-import { BsInstagram, BsTelephone, BsWhatsapp } from "react-icons/bs"
-import { FaFacebookMessenger } from "react-icons/fa"
+import { BiMessage } from "react-icons/bi"
 import contactPic from "../src/assets/contact-img.png"
+import { BsTelephone, BsWhatsapp } from "react-icons/bs"
+import { CiLocationOn } from "react-icons/ci"
+import { FaFacebookMessenger, FaInstagram } from "react-icons/fa"
 
 const ContactUs = () => {
   return (
-    <section id="contact-us" className="px-32 py-16">
-        <h1 className="font-bold text-yellow-400 lg:text-left text-center w-fit">CONTACT US</h1>
-        <div className="font-bold text-4xl py-4 col-span-5">Write To Us & Build</div>
-        <div className="grid lg:grid-cols-2 lg:gap-16 py-4 sm:px-0 px-4">
-            <img src={contactPic} alt="" className="sm:flex hidden"/>
-            <div className="grid lg:grid-cols-2 text-center gap-6">
-                <div className="grid border border-gray-200 shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
-                    <BiLocationPlus className="text-3xl mx-auto grid mt-6"/>
-                    <h1 className="text-2xl font-semibold">We are here ↓</h1>
-                    <h1 className="text-gray-600 text-sm">Nigeria, Africa</h1>
-                    <h2 className="text-gray-600 text-sm lg:pb-0 pb-8">BCon, Garden City</h2>
+    <section id="contact-us" className="md:px-32 sm:px16 px-12 py-16">
+        <h1 className="font-bold text-yellow-400 lg:text-left text-center">CONTACT US</h1>
+        <div className="font-bold text-4xl py-4">Write To Us & Build</div>
+        <div className="grid lg:grid-cols-2 gap-16 py-4">
+            <img src={contactPic} alt="" />
+            <div className="grid lg:grid-cols-2 gap-12 sm:mx-0 mx-auto">
+                <div className="">
+                    <div className="px-12 bg-white rounded-sm">
+                        <CiLocationOn className="text-white bg-black rounded-full text-4xl"/>
+                        <h1 className="font-semibold text-2xl py-6">We are here</h1>
+                        <p className="text-gray-600 text-sm">Nigeria, Afrcia</p>
+                        <p className="text-gray-600 text-sm">Graden City</p>
+                    </div>
                 </div>
-                <div className="grid border border-gray-200 shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
-                    <BsTelephone className="text-3xl mx-auto grid mt-6"/>
-                    <h1 className="text-2xl font-semibold">Talk To Us</h1>
-                    <h1 className="text-gray-600 text-sm lg:pb-0 pb-8">+234 802 888 4444</h1>
+                <div className="">
+                    <div className="px-12 bg-white rounded-sm">
+                        <BsTelephone className="text-white bg-black rounded-full text-4xl"/>
+                        <h1 className="font-semibold text-2xl py-6">Talk to us</h1>
+                        <p className="text-gray-600 text-sm">+23412312312345</p>
+                    </div>
                 </div>
-                <div className="grid border border-gray-200 shadow-lg hover:border-orange-300 hover:cursor-pointer hover:shadow-lg">
-                    <BiMessage className="text-3xl mx-auto grid mt-6"/>
-                    <h1 className="text-2xl font-semibold">Chat With Us</h1>
-                    <div className="text-gray-600 text-sm flex justify-between lg:px-18 px-44 lg:pt-0 pt-2  lg:pb-0 pb-8">
-                        <BsWhatsapp className="hover:text-orange-300 hover:shadow-md"/>
-                        <FaFacebookMessenger className="hover:text-orange-300 hover:shadow-md"/>
-                        <BsInstagram className="hover:text-orange-300 hover:shadow-md"/>
+                <div>
+                    <div className="px-12 bg-white rounded-sm">
+                        <BiMessage className="text-white bg-black rounded-full text-4xl"/>
+                        <h1 className="font-semibold text-2xl py-6">Have a chat</h1>
+                        <div className="text-gray-600 text-sm flex gap-6">
+                            <BsWhatsapp className="cursor-pointer hover:text-orange-300"/>
+                            <FaFacebookMessenger className="cursor-pointer hover:text-orange-300"/>
+                            <FaInstagram className="cursor-pointer hover:text-orange-300"/>
+                        </div>
                     </div>
                 </div>
             </div>
